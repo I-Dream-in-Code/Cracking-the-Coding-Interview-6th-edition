82be74be268c1e3e3ca3ac86c241922bad763ad3
Mike Harris <mike.a.harris@outlook.com>
2016-07-24 15:20:09 -0700

    fix without data type

--- Java/1/Is_Unique.java
+++ Java/1/Is_Unique.java
@@ -10,18 +10,14 @@ package Is_Unique;
 public class Is_Unique {
    public static boolean is_unique(String S){
         
-  	if (S.length() > 128) {
-			return false;
-		}
-		boolean[] char_set = new boolean[128];
-		for (int i = 0; i < S.length(); i++) {
-			int val = S.charAt(i);
-			if (char_set[val]) return false;
-			char_set[val] = true;
-		}
-		return true;
-    }
-    
+  	for(int i=0;i<S.length();i++){
+            if(S.indexOf(S.charAt(i))<i){
+                return false;
+            }
+        }    
+        return true;
+        
+        }
     public static void main(String[] args) {
         System.out.println(is_unique("abcd"));
         System.out.println(is_unique("aaa"));
